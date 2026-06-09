336bfcc2a044befa412656ed95a3cbc8d001dede
AKPANON Ange-Miguel <angemiguelosbelakpanon@gmail.com>
2026-06-09 13:40:28 +0100

    Delete serializers.py

--- serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Notification
-
-class NotificationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Notification
-        fields = ['id', 'type_notification', 'message', 'lu', 'date_creation']
-        read_only_fields = ['id', 'date_creation']
